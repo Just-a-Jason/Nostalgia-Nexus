58c66e69bd2dc3aa6a6d6f676fc7ae6873ee4455
= <antek.wojcieszyk@gmail.com>
2024-08-27 02:43:39 +0200

    dodano sklepik nemo

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,2 @@
 export const APPS_URL =
-  "https://raw.githubusercontent.com/Just-a-Jason/RepositoryForData/main/Apps.json";
+  "https://raw.githubusercontent.com/Just-a-Jason/Nostalgia-Nexus/main/apps.json";
