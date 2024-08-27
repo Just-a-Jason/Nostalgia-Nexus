63feb1707aee87ea6b70a001dec9bdb004999b2c
= <antek.wojcieszyk@gmail.com>
2024-08-27 21:45:30 +0200

    google drive downloader

--- src/API/GoogleDriveService.ts
+++ src/API/GoogleDriveService.ts
@@ -6,6 +6,7 @@ import {
 import { appDataDir } from "@tauri-apps/api/path";
 import { getClient, ResponseType } from "@tauri-apps/api/http";
 import { createDir } from "@tauri-apps/api/fs";
+import { VIRUS_WARNING_REGEX } from "../constants";
 
 interface DownloadOptions {
   onProgressCallback: (progress: { loaded: number; total: number }) => void;
@@ -13,50 +14,70 @@ interface DownloadOptions {
 }
 
 export default class GoogleDriveService {
+  constructor(private savePath: string) {}
   public async downloadFile(
     fileId: string,
     options: DownloadOptions
   ): Promise<void> {
-    const DOWNLOAD_URL = `https://drive.google.com/uc?id=${fileId}&export=download`;
+    const DOWNLOAD_DRIVE_URL = `https://drive.google.com/uc?id=${fileId}&export=download`;
+    this.savePath = `${await appDataDir()}apps\\${options.fileName}`;
     const BASE_PATH = (await appDataDir()) + "apps";
 
-    console.log(DOWNLOAD_URL);
+    await this.createAppsFolder(BASE_PATH);
+    if (await exists(this.savePath)) return;
 
-    if (!(await exists(BASE_PATH))) {
-      console.info(`Folder does not exist, creating... ${BASE_PATH}`);
-      await createDir(BASE_PATH, { recursive: true });
-    } else {
-      console.info("Folder already exists");
-    }
+    await this.downloadAppFiles(DOWNLOAD_DRIVE_URL);
+  }
 
-    const SAVE_PATH = `${await appDataDir()}apps\\${options.fileName}`;
-
-    console.log(SAVE_PATH);
-
-    if (await exists(SAVE_PATH)) return;
-
+  private async downloadAppFiles(downloadURL: string) {
     try {
       const client = await getClient();
-      const response = await client.get(DOWNLOAD_URL, {
+      const response = await client.get(downloadURL, {
         responseType: ResponseType.Binary,
       });
       const data = response.data;
 
-      await writeBinaryFile(SAVE_PATH, data as BinaryFileContents);
-
-      if (options.onProgressCallback) {
-        options.onProgressCallback({
-          loaded: (data as Uint8Array).length,
-          total: (data as Uint8Array).length,
-        });
-      }
-
-      console.info(`File downloaded successfully to ${SAVE_PATH}`);
-    } catch (error) {
-      console.warn(
-        `Something went wrong while accessing: ${DOWNLOAD_URL}`,
-        error
+      const dataString = new TextDecoder("utf-8").decode(
+        new Uint8Array(data as any).buffer
       );
+
+      if (this.isAntiVirusScreen(dataString)) {
+        await this.bypassAntivirusScreen(dataString);
+        return;
+      }
+    } catch (error) {
+      console.warn(error);
     }
+  }
+
+  private isAntiVirusScreen(data: string): boolean {
+    return VIRUS_WARNING_REGEX.test(data);
+  }
+
+  private async createAppsFolder(path: string) {
+    if (!(await exists(path))) {
+      console.info(`Folder does not exist, creating... ${path}`);
+      await createDir(path, { recursive: true });
+    }
+  }
+
+  private async bypassAntivirusScreen(htmlPage: string) {
+    const idRegex = /name="id" value="([^"]+)"/;
+    const confirmRegex = /name="confirm" value="([^"]+)"/;
+    const uuidRegex = /name="uuid" value="([^"]+)"/;
+
+    const id = idRegex.exec(htmlPage)?.[1];
+    const confirm = confirmRegex.exec(htmlPage)?.[1];
+    const uuid = uuidRegex.exec(htmlPage)?.[1];
+
+    if (id && confirm && uuid) {
+      const downloadUrl = `https://drive.usercontent.google.com/download?id=${id}&export=download&confirm=${confirm}&uuid=${uuid}`;
+
+      this.downloadZip(downloadUrl);
+    }
+  }
+
+  private async downloadZip(downloadURL: string) {
+    console.log(`Starting downloading zip files from: ${downloadURL}`);
   }
 }
