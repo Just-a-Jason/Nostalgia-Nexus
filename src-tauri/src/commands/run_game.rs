b7d66449dc0ecb000a6e4c256bef9f8f814c74ce
= <antek.wojcieszyk@gmail.com>
2024-10-24 23:42:20 +0200

    elapsed time between playthrough window emmision

--- src-tauri/src/commands/run_game.rs
+++ src-tauri/src/commands/run_game.rs
@@ -2,10 +2,11 @@ use std::os::windows::process::CommandExt;
 use std::process::{Command, Stdio};
 use std::time::Instant;
 use tauri::command;
+use tauri::Window;
 use winapi::um::winbase::CREATE_NO_WINDOW;
 
 #[command]
-pub async fn run_game(dir_path: String) -> Result<(), String> {
+pub async fn run_game(window: Window, dir_path: String) -> Result<(), String> {
     let result = Command::new("cmd")
         .args(&["/C", "cd", &dir_path, "&&", "game.exe"])
         .creation_flags(CREATE_NO_WINDOW)
@@ -14,12 +15,31 @@ pub async fn run_game(dir_path: String) -> Result<(), String> {
         .spawn();
 
     if let Ok(mut child_process) = result {
+        // Tell the launcher wich game started
+        window
+            .emit("game-started", {
+                serde_json::json!({
+                    "gameName": "idk"
+                })
+            })
+            .map_err(|e| e.to_string())?;
+
         // Start the timer
         let timer = Instant::now();
         let _ = child_process.wait();
 
         // The game has been closed, now calculate the time in-game
         println!("{:?}", timer.elapsed());
+
+        // Tell the launcher wich game ended
+        window
+            .emit("game-ended", {
+                serde_json::json!({
+                    "gameName": "idk",
+                    "totalPlayTime": timer.elapsed()
+                })
+            })
+            .map_err(|e| e.to_string())?;
     }
 
     Ok(())
