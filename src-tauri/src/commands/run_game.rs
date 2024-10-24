44f45b3c648f1ed26268bd94170f01e748e8305a
= <antek.wojcieszyk@gmail.com>
2024-10-24 23:36:13 +0200

    time in game calculation

--- src-tauri/src/commands/run_game.rs
+++ src-tauri/src/commands/run_game.rs
@@ -1,20 +1,26 @@
 use std::os::windows::process::CommandExt;
 use std::process::{Command, Stdio};
+use std::time::Instant;
 use tauri::command;
-use tauri::Window;
 use winapi::um::winbase::CREATE_NO_WINDOW;
 
 #[command]
-pub async fn run_game(window: Window, dir_path: String) -> Result<(), String> {
-    Command::new("cmd")
-        .args(&["/C", "cd", &dir_path, "&&", "start", "game.exe"])
+pub async fn run_game(dir_path: String) -> Result<(), String> {
+    let result = Command::new("cmd")
+        .args(&["/C", "cd", &dir_path, "&&", "game.exe"])
         .creation_flags(CREATE_NO_WINDOW)
         .stdout(Stdio::null())
         .stderr(Stdio::null())
-        .spawn()
-        .map_err(|e| e.to_string())?;
-    window
-        .emit("game-started", "Game started successfully")
-        .unwrap();
+        .spawn();
+
+    if let Ok(mut child_process) = result {
+        // Start the timer
+        let timer = Instant::now();
+        let _ = child_process.wait();
+
+        // Game closed time to check the time spent
+        println!("{:?}", timer.elapsed());
+    }
+
     Ok(())
 }
